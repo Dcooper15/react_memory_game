4ecd293c3f2f8c1719fdb1098c3b0f9999319a48
Dylan Cooper <dylancooper155@gmail.com>
2020-10-07 11:06:29 -0400

    finished part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import MemoryCard from './componets/MemoryCard'
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Memory Game</h1>
+        <h2 className="subtitle">Match Cards to Win</h2>
+        </header>
+        <div>
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+      </div>
+      <div>
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+      </div>
+      <div>
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+      </div>
+      <div>
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+        <MemoryCard />
+      </div>
     </div>
+    
   );
 }
 
